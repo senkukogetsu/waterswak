704d468db6deb23048f5e252826a3ea83df30de7
senkukogetsu <senkukogetsu98738@gmail.com>
2022-10-11 17:10:40 +0800

    input coordinate auto output

--- train.py
+++ train.py
@@ -22,8 +22,10 @@ temp = "point_catchment_csv"," ",'"'+name+'"'
 file = ''.join(temp)
 
 # for point_catchment
-coordinate_1 = 200998.5
-coordinate_2 = 2632927.8
+# coordinate_1 = 200998.5
+# coordinate_2 = 2632927.8
+coordinate_1 = sys.argv[2]
+coordinate_2 = sys.argv[3]
 temp = "point_catchment"," ",str(coordinate_1),",",str(coordinate_2)
 coordinate = ''.join(temp)
 # print(coordinate)
