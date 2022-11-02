724052294616f940bb846ca105fd7500257e85a2
senkukogetsu <senkukogetsu98738@gmail.com>
2022-11-02 17:16:42 +0800

    only catchment

--- auto.py
+++ auto.py
@@ -44,21 +44,21 @@ if __name__ =='__main__':
     gc.CLI.cli_cx.do_desc(line = "")
     print("\n########## Success！ ##########")
 
-    print("\n---------- output stream ----------")
-    gc.CLI.cli_cx.do_output(line = "stream")
-    print("\n########## Success！ ##########")
+    # print("\n---------- output stream ----------")
+    # gc.CLI.cli_cx.do_output(line = "stream")
+    # print("\n########## Success！ ##########")
 
-    print("\n---------- output subbas ----------")
-    gc.CLI.cli_cx.do_output(line = "subbas")
-    print("\n########## Success！ ##########")
+    # print("\n---------- output subbas ----------")
+    # gc.CLI.cli_cx.do_output(line = "subbas")
+    # print("\n########## Success！ ##########")
 
-    print("\n---------- output point_catchment_csv ----------")
-    try:
-        gc.CLI.cli_cx.do_output(line = file)
-        print("\n########## Save to CSV Success！ ##########")
-    except Exception as e:
-        print("\n########## Error Message ##########\n ", e)
-        print("\n########## Save to CSV Failed！ ##########")
+    # print("\n---------- output point_catchment_csv ----------")
+    # try:
+    #     gc.CLI.cli_cx.do_output(line = file)
+    #     print("\n########## Save to CSV Success！ ##########")
+    # except Exception as e:
+    #     print("\n########## Error Message ##########\n ", e)
+    #     print("\n########## Save to CSV Failed！ ##########")
 
     print("\n---------- output point_catchment ----------")
     try:
@@ -68,17 +68,17 @@ if __name__ =='__main__':
         print("\n########## Error Message ##########\n ", e)
         print("\n########## Save to CSV Failed！ ##########")
 
-    print("\n---------- output path ----------")
-    gc.CLI.cli_cx.do_output(line = path_name)
-    print("\n########## Success！ ##########")
+    # print("\n---------- output path ----------")
+    # gc.CLI.cli_cx.do_output(line = path_name)
+    # print("\n########## Success！ ##########")
 
-    print("\n---------- output nx_write_shp ----------")
-    gc.CLI.cli_cx.do_output(line = "nx_write_shp")
-    print("\n########## Success！ ##########")
+    # print("\n---------- output nx_write_shp ----------")
+    # gc.CLI.cli_cx.do_output(line = "nx_write_shp")
+    # print("\n########## Success！ ##########")
 
-    print("\n---------- output pathline_interpolate 10 ----------")
-    gc.CLI.cli_cx.do_output(line = "pathline_interpolate 10")
-    print("\n########## Success！ ##########")
+    # print("\n---------- output pathline_interpolate 10 ----------")
+    # gc.CLI.cli_cx.do_output(line = "pathline_interpolate 10")
+    # print("\n########## Success！ ##########")
     
     
         
